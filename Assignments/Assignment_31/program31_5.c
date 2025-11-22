d0f6b085393a9d7d0860a479ac7b3f6e0ffe5e15
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2025-11-22 23:39:35 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program31_5.c
@@ -0,0 +1,78 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header Files
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>               // For Input Output 
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Discription :   It is used to display the pattern 
+//  Input :         Integer, Integer
+//  Output :        Void
+//  Author :        Aditya Mali
+//  Date :          22/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input");
+        printf("Row and columan number should be same");
+        return;
+    }
+
+    for (i = 1; i <= iRow; i++)
+    {
+       for(j = 1; j <= iCol; j++)
+       {
+            if (i<=j)
+            {
+                printf("%d\t", j);
+            }
+            else
+            {
+                printf("\t");
+            }
+       }
+       printf("\n");
+    }
+}   // End of Pattern
+
+//  O(N)
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entory Point Function for the application
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows and Columns \n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}   // End of main
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  input1 : 5
+//  input2 : 5
+//  Output : 
+//              1       2       3       4       5
+//                      2       3       4       5
+//                              3       4       5
+//                                      4       5
+//                                              5
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
